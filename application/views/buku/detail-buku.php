4fce96e6579cbcc3dda5f426beac7fc67f3df3de
Meliyana707 <yanameli1707@gmail.com>
2022-04-13 22:10:14 +0700

    update pertemuan 3 dan 4

--- application/views/buku/detail-buku.php
+++ application/views/buku/detail-buku.php
@@ -1,15 +1,17 @@
 <div class="x_panel" align="center">
+
     <div class="x_content">
         <div class="row">
             <div class="col-sm-3 col-md-3">
-                <div class="thumbnail" style="height: auto; position: relative; left: 100%; width: 200%;">
+                <div class="thumbnail" style="height: auto; position: relative; left: 1 00%; width: 200%;">
                     <img src="<?php echo base_url(); ?>assets/img/upload/<?= $gambar; ?>" style="max-width:100%; max-height: 100%; height: 150px; width: 120px">
+
                     <div class="caption">
                         <h5 style="min-height:40px;" align="center"><?= $pengarang ?></h5>
                         <center>
                             <table class="table table-stripped">
                                 <tr>
-                                    <th nowrap>Judul Buku: </th>
+                                    <th nowrap>Judul Buku: </th>                                                                
                                     <td nowrap><?= $judul; ?></td>
                                     <td>&nbsp;</td>
                                     <th>Kategori: </th>
@@ -38,13 +40,14 @@
                                 </tr>
                             </table>
                         </center>
-                    <p>
-                        <a class="btn btn-outline-primary fas fw fa-shopping-cart" href="<?= base_url('booking/tambahBooking/' . $id); ?>"> Booking</a>
-                        <span class="btn btn-outline-secondary fas fw fa-reply" onclick="window.history.go(-1)"> Kembali</span>
-                    </p>
+                        <p>
+                            <a class="btn btn-outline-primary fas fw fa-shopping-cart" href="<?= base_url('booking/tambahBooking/' . $id); ?>"> Booking</a>
+                            <span class="btn btn-outline-secondary fas fw fa-reply" onclick="window.history.go(-1)"> Kembali</span>
+                        </p>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     </div>
+
 </div>
